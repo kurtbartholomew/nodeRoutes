ecbd7b1c707f1c90e5fc78a1e8c17e405c22afb1
Kurt Bartholomew <siever246@gmail.com>
2015-02-16 14:16:54 -0800

    Added test script to package.json for easy mocha test shorthand. Tests added.

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
-	throw 'Error'
-	//response.send('OK');
+	response.send('OK');
 });
 
 module.exports = app;
