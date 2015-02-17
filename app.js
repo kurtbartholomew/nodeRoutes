117f7ab7154c1821f4e8a44f1706c514b2ddf812
Kurt Bartholomew <siever246@gmail.com>
2015-02-17 11:45:37 -0800

    Changed package.json so I can use its test script instead. Also added redis and am using it for tests/app

--- app.js
+++ app.js
@@ -5,20 +5,27 @@ var urlencode = bodyParser.urlencoded({ extended:false });
 
 app.use(express.static('public'));
 
-var cities = {
-  'Lotopia' : 'some description',
-  'Caspiana' : 'description',
-  'Indigo' : 'description2'
-};
+var redis = require('redis');
+var client = redis.createClient();
+
+client.select((process.env.NODE_ENV || 'development').length);
+
+// client.hset('cities', 'Lotopia', 'description');
+// client.hset('cities', 'Lotopia', 'description');
+// client.hset('cities', 'Lotopia', 'description');
 
 app.get('/cities', function(request, response) {
-  response.json(Object.keys(cities));
+  client.hkeys('cities', function(error, names) {
+    response.json(names);
+  });
 });
 
 app.post('/cities', urlencode, function(request, response) {
   var newCity = request.body;
-  cities[newCity.name] = newCity.description;
-  response.status(201).json(newCity.name);
+  client.hset('cities', newCity.name, newCity.description, function(error){
+    if(error) throw error;
+    response.status(201).json(newCity.name);
+  })
 });
 
 module.exports = app;
